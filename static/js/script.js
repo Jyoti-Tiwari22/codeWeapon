d55109c4f2a310675e8d3df2cc5f302a3ab40ee3
jyoti-tiwari22 <tiwarij657@gmail.com>
2020-07-17 20:41:38 +0530

    template: a beautiful three box contact form

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('parentDiv').style.position = 'relative';
+document.getElementById('top-drop').style.position = 'absolute';
+document.getElementById('second-drop').style.position = 'absolute';
+document.getElementById('last-drop').style.position = 'absolute';
+document.getElementById('second-drop').style.zIndex= '2';
